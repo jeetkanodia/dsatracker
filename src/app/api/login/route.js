1fe91380f19e73d6354bb7b379a1d841f8da5745
jeet kanodia <jeetkanodia@gmail.com>
2023-12-17 20:13:54 +0530

    add login and signup backend

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,19 +1,46 @@
 import { MongoClient } from "mongodb";
 import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET; // Replace with a secure secret key
 
 export async function POST(req, res) {
   try {
-    const client = new MongoClient(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const client = new MongoClient(process.env.MONGODB_URI);
     await client.connect();
     const db = client.db();
 
-    const newPost = await req.json();
-    const result = await db.collection("emails").insertOne(newPost);
+    const { email, password } = await req.json();
+    if (!email || !password) {
+      return NextResponse.json({ error: "Missing email or password" });
+    }
+
+    //check if email exists
+    const userExists = await db.collection("users").findOne({ email });
+    if (!userExists) {
+      return NextResponse.json({ error: "Email does not exist" });
+    }
+
+    // check if the password is valid
+    const validPassword = await bcrypt.compare(password, userExists.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid password" });
+    }
+
+    const result = {
+      email: userExists.email,
+      username: userExists.username,
+    };
+
+    // Create a JWT token
+    const token = jwt.sign({ email: userExists.email }, JWT_SECRET, {
+      expiresIn: "1d", // Set an expiration time for the token
+    });
+
     await client.close();
-    return NextResponse.json(result);
+
+    return NextResponse.json({ result, token });
   } catch (error) {
     return NextResponse.json({ error });
   }
---
+++ src/app/api/register/route.js
@@ -0,0 +1,78 @@
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export async function POST(req, res) {
+  try {
+    const client = new MongoClient(process.env.MONGODB_URI);
+    await client.connect();
+    const db = client.db();
+
+    const { username, email, password } = await req.json();
+    if (!email || !password || !username) {
+      return NextResponse.json({
+        error: "Missing email ,password or username",
+      });
+    }
+
+    // check if the email is valid
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(email)) {
+      return NextResponse.json({ error: "Invalid email" });
+    }
+
+    // Check if the user already exists by email
+    const existingUserByEmail = await db.collection("users").findOne({ email });
+    if (existingUserByEmail) {
+      return NextResponse.json({ error: "Email already in use" });
+    }
+
+    // Check if the user already exists by username
+    const existingUserByUserName = await db
+      .collection("users")
+      .findOne({ username });
+    if (existingUserByUserName) {
+      return NextResponse.json({ error: "Username already in use" });
+    }
+
+    // {
+    //     "email": "Test@Test.com",
+    //     "username" : "Jeet kanodia",
+    //     "password": "Test1234"
+    // }
+
+    // check if the password is valid (min 8 chars, 1 number, 1 uppercase, 1 lowercase and 1 special character)
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    if (!passwordRegex.test(password)) {
+      return NextResponse.json({
+        error:
+          "Password must be at least 8 characters long, contain at least 1 number, 1 lowercase and 1 uppercase letter and a special character",
+      });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = {
+      username,
+      email,
+      password: hashedPassword,
+    };
+
+    const result = await db.collection("users").insertOne(newUser);
+
+    // Create a JWT token
+    const token = jwt.sign({ email: newUser.email }, JWT_SECRET, {
+      expiresIn: "1d", // Set an expiration time for the token
+    });
+
+    await client.close();
+
+    return NextResponse.json({ username, email, result, token });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
