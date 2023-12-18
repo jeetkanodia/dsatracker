a2df91c27bd96d51a88c915bc8e33b80e4dd59fd
jeet kanodia <jeetkanodia@gmail.com>
2023-12-19 02:07:57 +0530

    Update next.config.js and Loader component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,11 +2,13 @@
 import React, { useState, useEffect } from "react";
 import Card from "./component/Card/Card";
 import { set } from "mongoose";
+import Loader from "./component/Loader/Loader";
 
 export default function ButtonUsage() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+
   useEffect(() => {
     setLoading(true);
     fetch("/api/topics")
@@ -27,19 +29,25 @@ export default function ButtonUsage() {
   return (
     <>
       <div className="bg-[#212121] w-full min-h-screen h-auto flex flex-col items-center sm:pt-[100px] pt-[70px]">
-        <h1 className="home-title text-5xl font-bold text-white">
-          Crack DSA with our specialized curated list🔥
-        </h1>
-        <div className="flex flex-wrap w- items-center ">
-          {data.map((item) => (
-            <Card
-              key={item.title}
-              title={item.title}
-              link={item.link}
-              number={item.number}
-            />
-          ))}
-        </div>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <h1 className="home-title text-5xl font-bold text-white">
+              Crack DSA with our specialized curated list🔥
+            </h1>
+            <div className="flex flex-wrap w- items-center ">
+              {data.map((item, idx) => (
+                <Card
+                  key={idx}
+                  title={item.title}
+                  link={item.link}
+                  number={item.number}
+                />
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </>
   );
