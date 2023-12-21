65c64a86e26aabb15dca50cce94175543e0b1ae4
jeet kanodia <jeetkanodia@gmail.com>
2023-12-21 18:37:00 +0530

    Refactor Card component styling and update button library

--- src/app/component/Card/Card.jsx
+++ src/app/component/Card/Card.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Link from "next/link";
-import "./Card.css";
-import Button from "@mui/material/Button";
+import { Button, ButtonGroup } from "@nextui-org/react";
+
+// import Button from "@mui/material/Button";
 
 const Card = ({ link, title, number }) => {
   // change title to sentence case
@@ -12,24 +13,23 @@ const Card = ({ link, title, number }) => {
     .join(" ");
 
   return (
-    <div className="card-container">
-      <h1 className="card-title">{Title}</h1>
-      <p className="card-numbers">Total questions: {number}</p>
-
-      <div className="card-btn">
+    <div className="bg-gradient-to-br from-[#957AFF] m-auto p-2 hover:scale-105 shadow-sm transition ease-in-out from-0% to-[#7855FF] to-90%  flex flex-col items-start rounded-[10px]">
+      <div className="">
+        <h1 className="text-white  text-3xl">{Title}</h1>
+        <p className="text-sm text-[rgba(255,255,255,0.5)]">
+          Total questions: {number}
+        </p>
+      </div>
+      <p className="w-full mr-8">
         <Link href={`questions/${link}`}>
           <Button
-            style={{
-              borderRadius: "30px",
-              textTransform: "none",
-            }}
-            color="secondary"
-            variant="contained"
+            variant="flat"
+            className="bg-white text-[#6842FF] rounded-md w-full h-7"
           >
             Start Solving
           </Button>
         </Link>
-      </div>
+      </p>
     </div>
   );
 };
